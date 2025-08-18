a54ffb2d895bd07f4c41ff2bf14b18d1cab02f3c
Muhammad Zaeem Tauqir <zaeem.tauqir@unikrew.com>
2025-08-18 12:46:58 +0500

    Add: Graceful shutdown complete

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/ZaeemT/GO-microservice/application"
 )
@@ -11,7 +13,10 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app: ", err)
 	}
